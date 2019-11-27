c33b460c32f47c131c9e9194b61a1798ca3fd7f3
Kaz <itskazborn@gmail.com>
2019-11-27 15:08:35 -0300

    Map class

---
+++ Jogos!/Jogos!/Model/Map.swift
@@ -0,0 +1,29 @@
+//
+//  Map.swift
+//  Jogos!
+//
+//  Created by Kaz Born on 27/11/19.
+//  Copyright © 2019 Kelvin Javorski Soares. All rights reserved.
+//
+
+import Foundation
+
+class Map {
+	var map : [[Place]] = [[]]
+	
+	var playersPosition : [Int] = [3, 3]
+	
+	
+	func generateMap (size: Int) {
+		
+	}
+	
+	
+	func distanceBetween(pos1 : [Int], pos2 : [Int]) {
+		
+	}
+	
+	
+	
+	
+}
