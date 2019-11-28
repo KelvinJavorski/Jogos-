61c5f7b729f86377971ceaf1d8803e38c7a32710
Kelvin <kelvin.javorskisoares@gmail.com>
2019-11-28 13:59:28 -0300

    Criado Storyboard Setup para preencher os nomes dos Jogadores
    Adicionado Storyboard e view controller para a tela de preenchimento de nomes / numero de jogadores

---
+++ Jogos!/Jogos!/Controllers/SetupViewController.swift
@@ -0,0 +1,87 @@
+//
+//  SetupViewController.swift
+//  Jogos!
+//
+//  Created by Kelvin Javorski Soares on 27/11/19.
+//  Copyright © 2019 Kelvin Javorski Soares. All rights reserved.
+//
+
+import UIKit
+
+class SetupViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
+
+       
+    @IBOutlet weak var nameText : UITextField!
+    @IBOutlet weak var numberOfPlayerText : UITextField!
+    @IBOutlet weak var confirmButton: UIButton!
+    
+    var numberOfPlayers = 0
+    var currentNumber = 0
+    let picker = UIPickerView()
+    let toolBar = UIToolbar()
+
+
+    let availablePlayersOption = [3,4,5]
+    var players = [Player]()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        createNumberOfPlayersPicker()
+        picker.delegate = self
+        picker.dataSource = self
+        print(numberOfPlayers)
+        
+        // Do any additional setup after loading the view.
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        view.endEditing(true)
+        super.touchesBegan(touches, with: event)
+    }
+    
+    func createNumberOfPlayersPicker(){
+//        picker.backgroundColor = .white
+        picker.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(picker)
+        picker.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor).isActive = true
+        picker.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor).isActive = true
+        picker.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor).isActive = true
+    }
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return availablePlayersOption.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        numberOfPlayers = availablePlayersOption[row]
+        return "\(availablePlayersOption[row])"
+    }
+    
+    @IBAction func setNumberOfPlayers(_ sender: Any){
+        print(numberOfPlayers)
+    }
+    
+    @IBAction func createNewPlayerName(_ sender: Any){
+        if(nameText.text != ""){
+            players.append(Player(name: nameText.text!))
+            nameText.text = ""
+        }
+        
+        if (verifyMaxNumberWasReached()){
+            print("Players names have been filled")
+//            performSegue(withIdentifier: <#T##String#>, sender: <#T##Any?#>)
+        }
+    }
+    
+    func verifyMaxNumberWasReached() -> Bool{
+        if (players.count < numberOfPlayers){
+            return false
+        }
+        return true
+    }
+
+}
