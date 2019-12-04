8a362ea602e9a705f9abaecedbebf0232873c8d6
Kevin Ribeiro <kevin.ribeiro@pucpr.edu.br>
2019-12-04 16:20:37 -0300

    gitflow-feature-stash: posições_atualizado

--- Jogos!/Jogos!/Model/Map.swift
+++ Jogos!/Jogos!/Model/Map.swift
@@ -147,126 +147,43 @@ class Map {
 	
 	// >>>---------> DIRECTIONS
 	
-	func distanceBetween(pos1 : [Int], pos2 : [Int]) {
+	func distanceBetween(placePlayer : Place, placeTarget : Place) {
 		
 	}
-	
-    func Directions(placePlayer : Place, placeTarget : Place){
     
-        var angle = atan2f(Float(placePlayer.coordenate.y - placePlayer.coordenate.y), Float(placePlayer.coordenate.x - placePlayer.coordenate.x))
-        
-        if (angle < 0) { angle += 2 * Float.pi; }
-        
-        var angleIntervals : [[Float]] = [[]]
-        let startAngle = 0
-        let endAngle = 1
-        
-        for interval in 1...8 {
-            angleIntervals[interval][startAngle] = ((2*Float.pi)/8)*Float(interval) - (2*Float.pi)/16
-            angleIntervals[interval][endAngle] = angleIntervals[interval][startAngle] + (2*Float.pi)/8 - (2*Float.pi)/16
-            
-            DirectionCorrection(angle: angle, angleIntervals: angleIntervals, interval: interval, startAngle: startAngle, endAngle: endAngle)
-        }
-        
-    }
-    
-    func DirectionCorrection(angle : Float, angleIntervals : [[Float]], interval : Int, startAngle : Int, endAngle : Int){
-        
-        if (angle < angleIntervals[interval][endAngle] && angle > angleIntervals[interval][startAngle]) {
-            
-            switch interval {
-            case 1:
-                direction.self = directions.east
-                break
-            case 2:
-                direction.self = directions.northeast
-                break
-            case 3:
-                direction.self = directions.north
-                break
-            case 4:
-                direction.self = directions.northwest
-                break
-            case 5:
-                direction.self = directions.west
-                break
-            case 6:
-                direction.self = directions.southwest
-                break
-            case 7:
+    func Directions(placePlayer : Place, placeTarget : Place) {
+        if (placePlayer.coordenate.x == placeTarget.coordenate.x) {
+            if (placePlayer.coordenate.y < placeTarget.coordenate.y) {
                 direction.self = directions.south
-                break
-            case 8:
-                direction.self = directions.southeast
-                break
-            default: break
+            }
+            else {
+                direction.self = directions.north
             }
         }
-        else if (angle == angleIntervals[interval][endAngle]){
-            
-            switch interval {
-            case 1:
-                direction.self = directions.northeast
-                break
-            case 2:
-                direction.self = directions.north
-                break
-            case 3:
-                direction.self = directions.northwest
-                break
-            case 4:
+        if (placePlayer.coordenate.y == placeTarget.coordenate.y) {
+            if (placePlayer.coordenate.x < placeTarget.coordenate.x) {
                 direction.self = directions.west
-                break
-            case 5:
-                direction.self = directions.southwest
-                break
-            case 6:
-                direction.self = directions.south
-                break
-            case 7:
-                direction.self = directions.southeast
-                break
-            case 8:
+            }
+            else {
                 direction.self = directions.east
-                break
-            default: break
             }
         }
-        else if (angle == angleIntervals[interval][startAngle]) {
-            
-            switch interval {
-            case 1:
-                direction.self = directions.southeast
-                break
-            case 2:
-                direction.self = directions.east
-                break
-            case 3:
-                direction.self = directions.northeast
-                break
-            case 4:
-                direction.self = directions.north
-                break
-            case 5:
+        if (placePlayer.coordenate.x < placeTarget.coordenate.x) {
+            if (placePlayer.coordenate.y < placeTarget.coordenate.y) {
                 direction.self = directions.northwest
-                break
-            case 6:
-                direction.self = directions.west
-                break
-            case 7:
+            }
+            else if (placePlayer.coordenate.y > placeTarget.coordenate.y) {
                 direction.self = directions.southwest
-                break
-            case 8:
-                direction.self = directions.south
-                break
-            default: break
+            }
+        }
+        else if (placePlayer.coordenate.x > placeTarget.coordenate.x) {
+            if (placePlayer.coordenate.y < placeTarget.coordenate.y) {
+                direction.self = directions.southeast
+            }
+            else if (placePlayer.coordenate.y > placeTarget.coordenate.y) {
+                direction.self = directions.northeast
             }
         }
     }
-
-    
-    
-    
-    
     
 }
