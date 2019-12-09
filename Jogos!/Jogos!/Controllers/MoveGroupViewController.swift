df8b7ae4202da0c5af4fd824c19fcd43332c1d2c
Kevin Ribeiro <kevin.ribeiro@pucpr.edu.br>
2019-12-09 16:58:40 -0300

    colocado os outlets do MoveGroup

---
+++ Jogos!/Jogos!/Controllers/MoveGroupViewController.swift
@@ -0,0 +1,77 @@
+//
+//  MoveGroupViewController.swift
+//  Jogos!
+//
+//  Created by Kevin Ribeiro on 09/12/19.
+//  Copyright © 2019 Kelvin Javorski Soares. All rights reserved.
+//
+
+import UIKit
+
+class MoveGroupViewController: UIViewController {
+    
+    
+    @IBOutlet weak var windRoseImage: UIImageView!
+    
+    @IBOutlet weak var Nbutton: UIButton!
+    @IBOutlet weak var NEbutton: UIButton!
+    @IBOutlet weak var Ebutton: UIButton!
+    @IBOutlet weak var SEbutton: UIButton!
+    @IBOutlet weak var Sbutton: UIButton!
+    @IBOutlet weak var SWbutton: UIButton!
+    @IBOutlet weak var Wbutton: UIButton!
+    @IBOutlet weak var NWbutton: UIButton!
+    
+    @IBOutlet weak var chooseDirectionButton: UIButton!
+    
+    
+    @IBAction func Naction(_ sender: Any) {
+    }
+    
+    @IBAction func NEaction(_ sender: Any) {
+    }
+    
+    @IBAction func Eaction(_ sender: Any) {
+    }
+    
+    @IBAction func SEaction(_ sender: Any) {
+    }
+    
+    @IBAction func Saction(_ sender: Any) {
+    }
+    
+    @IBAction func SWaction(_ sender: Any) {
+    }
+    
+    @IBAction func Waction(_ sender: Any) {
+    }
+    
+    @IBAction func NWaction(_ sender: Any) {
+    }
+    
+    
+    @IBAction func chooseDirectionAction(_ sender: Any) {
+    }
+    
+    
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
